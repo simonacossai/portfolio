68c2ac88c7d319ce2633e6d3ca169c32cc8b4c77
simonacossai <63307173+simonacossai@users.noreply.github.com>
2021-05-01 13:50:19 +0200

    DONE- work with responsiveness in about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,14 +9,14 @@ export default class About extends Component {
         return (
             <Container fluid className="about-container d-flex justify-content-center align-items-center">
                 <Row >
-                    <Col lg={6} md={12} className="about-left-col pr-5">
+                    <Col lg={6} md={12} className="about-left-col">
                         <img src={about} className="about-img" alt="about-me-image" />
                     </Col>
-                    <Col lg={6} md={12} style={{flexDirection: "column"}} className="pl-5 about-right-col text-left d-flex justify-content-center align-items-center">
+                    <Col lg={6} md={12} className="about-right-col text-left d-flex flex-column justify-content-center align-items-center">
                         <div className="about-text">
-                      <div className="d-flex " style={{justifyContent: "space-between"}}> 
+                      <div className="d-flex justify-content-between" > 
                       <h1 className="text-left">Get to know me!</h1>
-                      <img src={icon} className="mb-3" style={{width:"60px", height:"50px"}}/></div>  
+                      <img src={icon} className="mb-3 quote-img"/></div>  
                         <p>Hi! I'm Simona, a 19yo developer based in Naples, Italy. I started my journey in the web development world not really long ago even tho I've always been a tech enthusiast since I was a child.  </p>
                         <p>I started thanks to online courses and bootcamps, where I first got in touch with the MERN stack and the other technologies I'm currently working with. </p>
                         <p>If you're curious scroll and go take a look at my most recent projects -and why not- reach me out on Linkedin if you have any questions and let's have a chat! </p>
